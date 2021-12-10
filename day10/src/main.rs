af94ab67e5d3659ece0ad889c6d8d9b0eb14f816
baptr <baptr01@gmail.com>
2021-12-09 23:28:58 -0800

    day10.2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -12,26 +12,30 @@ fn is_terminal(l: &str) -> bool {
     }
 }
 
-fn parse(l: &str, open: &str) -> (usize, i64) {
-    println!("{} : {}: start", open, l);
-    if l.len() == 0 {
-        return (0, 0);
-    }
-    if is_terminal(open) { return (1, -1); }
-    let close = match open {
-        "(" => ")",
-        "[" => "]",
-        "{" => "}",
-        "<" => ">",
-        "EOL" => "EOL",
-        _ => return (0, 0),
+fn parse(l: &str, open: &str) -> (usize, u64, u64) {
+    if is_terminal(open) { return (1, 0, 0); }
+    let (close, complete_points) = match open {
+        "(" => (")", 1),
+        "[" => ("]", 2),
+        "{" => ("}", 3),
+        "<" => (">", 4),
+        "EOL" => ("EOL", 0),
+        _ => {
+            println!("Invalid open char in {} : {}", open, l);
+            return (0, 0, 0);
+        },
     };
+    println!("{} : {}: looking for {}", open, l, close);
+
     let mut pos = 0;
+    let mut child_score = 0;
     loop {
-        // XXX handle eol
-        if pos+1 >= l.len() {
-            println!("{} : {}: - exhausted @ {}", open, l, pos);
-            return (pos+1, 0);
+        if pos+1 > l.len() { // handle EOL
+            println!("{} : {}: - exhausted @ {}, adding {} for {} points", open, l, pos, close, complete_points);
+            if complete_points == 0 {
+                return  (0, 0, child_score);
+            }
+            return (pos+1, 0, complete_points+child_score*5);
         }
         let next = &l[pos..pos+1];
         if is_terminal(next) {
@@ -44,18 +48,19 @@ fn parse(l: &str, open: &str) -> (usize, i64) {
                     _ => 0,
                 };
                 println!("{} : {}: expected {} but found {} instead : {})", open, l, close, next, score);
-                return (pos+1, score);
+                return (pos+1, score, 0);
             }
             // matched pop
             println!("{} : {}: found {}, popping {}", open, l, next, pos+1);
-            return (pos+1, -1);
+            return (pos+1, 0, 0);
         }
 
-        let (skip, sub_score) = parse(&l[pos+1..], next);
-        println!("{} : {}: recurse at +{}={} returned: skip={} sub_score={}", open, l, pos, next, skip, sub_score);
-        if sub_score != -1 {
-            return (pos+skip, sub_score);
+        let (skip, sub_part1, sub_part2) = parse(&l[pos+1..], next);
+        println!("{} : {}: recurse at +{}={} returned: skip={} sub_part1={} sub_part2={}", open, l, pos, next, skip, sub_part1, sub_part2);
+        if sub_part1 > 0 {
+            return (pos+skip, sub_part1, sub_part2);
         }
+        child_score = 5*child_score + sub_part2;
         pos += skip + 1;
     }
 }
@@ -66,13 +71,19 @@ fn main() -> std::io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut part1 = 0;
+    let mut part2 = Vec::new();
     for line in reader.lines() {
         let l = line?;
-        let (skip, score) = parse(&l, "EOL");
-        println!("{} {}", skip, score);
-        part1 += score;
+        let (skip, incomplete_score, complete_score) = parse(&l, "EOL");
+        println!("{} done: skip={} fail={} finish={}", l, skip, incomplete_score, complete_score);
+        part1 += incomplete_score;
+        if complete_score > 0 {
+            part2.push(complete_score);
+        }
     }
+    part2.sort();
 
     println!("part1 total: {}", part1);
+    println!("part2 median: {}", part2[part2.len()/2]);
     Ok(())
 }
