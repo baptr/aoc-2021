f1c2fdc3343f0a401ace16fb540382d19c42f700
baptr <baptr01@gmail.com>
2021-12-13 02:12:37 -0800

    day13.2 take 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-use std::cmp;
+//use std::cmp;
 //use std::option::Option;
 //use std::cmp::Ordering;
 //use std::cell::Cell;
@@ -13,8 +13,6 @@ use std::cmp;
 //use std::collections::HashMap;
 //use itertools::Itertools;
 
-const DEBUG_FOLD: bool = false;
-
 fn dump(grid: &Vec<Vec<bool>>) {
     let h = 3;
     for (i, row) in grid.iter().enumerate() {
@@ -23,48 +21,11 @@ fn dump(grid: &Vec<Vec<bool>>) {
             if *v {
                 print!("#");
             } else {
-                print!(".");
+                print!(" ");
             }
         }
         println!();
     }
-}
-
-fn fold(grid: Vec<Vec<bool>>, crease: &(String, usize)) -> Vec<Vec<bool>> {
-    println!("doing fold {}={}, grid height={}", crease.0, crease.1, grid.len());
-    let mut out = Vec::new();
-    if crease.0 == "y" {
-        for i in 0..crease.1 {
-            out.push(grid[i].clone());
-        }
-        let end = cmp::min(grid.len(),2*crease.1+1);
-        for i in crease.1+1..end {
-            let o = 2*crease.1-i;
-            for (p, v) in grid[i].iter().enumerate() {
-                if *v {
-                    out[o][p] |= true;
-                }
-            }
-        }
-    } else {
-        for old in grid {
-            let mut row = Vec::new();
-            let end = cmp::min(crease.1, old.len());
-            row.extend(&old[..end]);
-            for i in 1..=crease.1 {
-            //for a in crease.1+1..old.len() {
-                let a = crease.1+i;
-                let b = crease.1-i;
-                //let b = old.len()-1-a;
-                if a >= old.len() { break; }
-                if b >= row.len() { continue; }
-                row[b] |= old[a];
-            }
-            out.push(row);
-        }
-    }
-    if DEBUG_FOLD { dump(&out); }
-    return out;
 }
 
 fn main() -> std::io::Result<()> {
@@ -72,11 +33,10 @@ fn main() -> std::io::Result<()> {
     let file = File::open(name)?;
     let reader = BufReader::new(file);
 
-    let mut grid = Vec::new();
-    let mut folds = Vec::new();
+    let mut points = Vec::new();
     let mut read_folds = false;
-    let mut max_y = 0;
-    let mut max_x = 0;
+    let mut last_x = 0;
+    let mut last_y = 0;
     for line in reader.lines() {
         let l = line?;
         if l.is_empty() {
@@ -86,56 +46,40 @@ fn main() -> std::io::Result<()> {
         if !read_folds {
             let (a, b) = l.split_once(',').unwrap();
             let x = a.parse::<usize>().unwrap();
-            if x > max_x { max_x = x; }
             let y = b.parse::<usize>().unwrap();
-            if y > max_y { max_y = y; }
-            if y >= grid.len() {
-                grid.resize(y+1,Vec::new());
-            }
-            if x >= grid[y].len() {
-                grid[y].resize(x+1, false);
-            }
-            grid[y][x] = true;
+            points.push((x, y));
         } else {
             let p = l.split_whitespace().last().unwrap();
             let (dir, pos) = p.split_once('=').unwrap();
-            folds.push((dir.to_string(), pos.parse::<usize>().unwrap()));
-        }
-    }
-    println!("max_y={} max_x={}", max_y, max_x);
-    for row in &mut grid {
-        row.resize(max_x+1, false);
-    }
-
-    println!("grid height: {} fold len: {}", grid.len(), folds.len());
-    if DEBUG_FOLD { dump(&grid); }
-
-    grid = fold(grid, &folds[0]);
-    let mut part1 = 0;
-    for row in &grid {
-        for v in row {
-            if *v {
-                part1 += 1;
-            }
-        }
-    }
-    println!("part1: {}", part1);
-
-    for f in &folds[1..] {
-        grid = fold(grid, &f);
-    }
-
-    println!("part2:");
-    for row in &grid {
-        for v in row {
-            if *v {
-                print!("#");
+            let v = pos.parse::<usize>().unwrap();
+            if dir == "x" {
+                last_x = v;
             } else {
-                print!(" ");
+                last_y = v;
             }
         }
-        println!("");
     }
+    println!("last_x={} last_y={}", last_x, last_y);
+
+    let mut grid = Vec::new();
+    grid.resize(last_y, Vec::new());
+    for row in &mut grid {
+        row.resize(last_x, false);
+    }
+
+    for (x, y) in points {
+        let x_seg = (x+1) / (last_x+1);
+        let mut i = (x+1) % (last_x+1) - 1;
+        if x_seg % 2 == 1 { i = last_x-i-1; }
+
+        let y_seg = (y+1) / (last_y+1);
+        let mut j = (y+1) % (last_y+1) - 1;
+        if y_seg % 2 == 1 { j = last_y-j-1; }
+
+        grid[j][i] = true;
+    }
+
+    dump(&grid);
 
     Ok(())
 }
